6ac2712b1affa00ed3b63cc4754c3be6013f52ad
fabiomrjr <nflnhl7>
2017-07-02 01:16:37 -0300

    Refatorações e inclusão de Experiências de carreira.

--- functions.js
+++ functions.js
@@ -5,19 +5,57 @@ $(document).ready(function() {
 		$('#job').text(data.job);
 		$('#textIntro').text(data.textIntro);
 		
-		$.each(data.skills, function(i, item){
-			addSkill(item);
-		});
-		
-		$.each(data.knowledges, function(i, item){
-			addKnowledge(item);
-		});
+		addSkillsLanguages(data);
+		addJobExperiences(data);
+    });
+});
+
+function addJobExperiences(data){
+	
+	var htmlParagrah = $("<tr><td class='col-xs-4 col-md-4'><div class='zeroPadding' id='professionalExperienceImage'><h4 class='text-right'><span class='fa fa-suitcase' aria-hidden='true'></span></h4></div></td><td class='col-xs-8 col-md-8'><div class='zeroPadding' id='professionalExperienceTitle'> <h4 class='text-left animated bounceInRight'>Professional Experience</h4></div></td></tr>");
+	
+	$('#tableCareer').append(htmlParagrah);
+	
+	$.each(data.JobExperiences, function(i, item){
+		addHead(item);
+		addBody(item);	
+	});
+}
+
+function addBody(JobExperience){
+	
+	var htmlParagrah = $("<tr><td class='col-xs-4 col-md-4'><div class='zeroPadding'></div></td><td class='col-xs-8 col-md-8 teste3'><div class='zeroPadding2' id='professionalExperienceCompany'> <h4 id='teste'>" + JobExperience.JobCompany + "</h4><h5 id='professionalExperienceCompanyText'>" + JobExperience.JobDescription + "</h5></div></td></tr>");
+	
+	$('#tableCareer').append(htmlParagrah);
+}
+
+function addHead(JobExperience){
+	var htmlParagrah = $("<tr><td class='col-xs-4 col-md-4'><h4 class='arrow_box teste45'>" + JobExperience.JobDate + "</h4></td><td class='col-xs-8 col-md-8 teste2'><div class='zeroPadding' id='professionalExperienceJob'><h4 id='professionalExperienceJobText'>" + JobExperience.Job + "</h4></div></td></tr>");
+	
+	$('#tableCareer').append(htmlParagrah);
+}
+
+function addSkillsLanguages(data){
+	if(data.skills.length === 1)
+		{
+			addSkill(data.skills[i-1], '#skillsCol1');
+			$('#skillsCol2').remove();
+		}
+		else{
+			for (var i = 1; i < data.skills.length + 1; i++) {
+				if(i % 2){
+					addSkill(data.skills[i-1], '#skillsCol1');
+				}
+				else{
+					addSkill(data.skills[i-1], '#skillsCol2');
+				}
+			}
+		}
 		
 		$.each(data.languages, function(i, item){
 			addLanguage(item);
 		});
-    });
-});
+}
 
 function addLanguage(item){
 	
@@ -32,28 +70,15 @@ function addLanguage(item){
 	animateProgressBar(idProgressBar, item.languageLevel);
 }
 
-function addKnowledge(item){
-	
-	var htmlParagrah = $("<h5></h5>").text(item.nameKnowledge);
-	var idProgressBar = "myBar" + item.nameKnowledge;
-		
-	var htmlProgressBar = $('<div class="progress"><div id="'+ idProgressBar + '" class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:0%"></div></div>');
-	
-	$('#knowledges').append(htmlParagrah);
-	$('#knowledges').append(htmlProgressBar);
-	
-	animateProgressBar(idProgressBar, item.knowledgeLevel);
-}
-
-function addSkill(item){
+function addSkill(item, col){
 	
 	var htmlParagrah = $("<h5></h5>").text(item.nameSkill);
 	var idProgressBar = "myBar" + item.nameSkill;
 		
 	var htmlProgressBar = $('<div class="progress"><div id="'+ idProgressBar + '" class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:0%"></div></div>');
 	
-	$('#skills').append(htmlParagrah);
-	$('#skills').append(htmlProgressBar);
+	$(col).append(htmlParagrah);
+	$(col).append(htmlProgressBar);
 	
 	animateProgressBar(idProgressBar, item.skillLevel);
 }
